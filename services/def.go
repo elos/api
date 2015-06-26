cc1eab77b728a5cab5e82ec78c4c486e02116612
Nick Landolfi <nclandolfi@gmail.com>
2015-06-25 17:29:29 -0700

    comment in services/def.go

--- services/def.go
+++ services/def.go
@@ -5,10 +5,16 @@ import (
 	"github.com/elos/data"
 )
 
+// The file contains the interface definition for elos services
+// There is only this one file because write these interfaces
+// are intermediaries to external services
+
+// DB is an intermediary for the data.dB type
 type DB interface {
 	data.DB
 }
 
+// Agents is an intermediary for the autonomous.Manager type
 type Agents interface {
 	autonomous.Manager
 }
